4630908793fcf0c4e94810da6c9bfade3eddde65
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-29 22:05:51 +0300

    while with fibonacci

---
+++ whilit.py
@@ -0,0 +1,20 @@
+x="hello world"
+while not len(x)==5:  #len is 11 more 6 to 5
+    x=x[1:]           #will keep on until the string is sliced 5 times in the form
+print(x)
+
+#using range function use input as starting range function
+#loop through each of the variable
+#fibonacci series,
+#for each variable each side 
+#i-1+i-2
+#Display 
+#End function
+def fibonacci(z):
+    for i in range(z,51):
+        while i<52:
+             y=i-1
+             w=i-2
+            
+       
+             print(f"{y},{w}")
